4bd784b0c8fa6c37f22f56e157c587cbb56898d1
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-20 23:40:34 -0600

    update register form - add state and input validation

--- src/components/landing-page/landing-page-form-register.tsx
+++ src/components/landing-page/landing-page-form-register.tsx
@@ -1,38 +1,56 @@
 import { Button, TextField } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useInput } from '../../utils/customHooks';
+import { emailValidator, passwordValidator } from '../../utils/validators';
 import styles from './landing-page-form.module.css';
 
 function LandingPageFormRegister() {
   const navigate = useNavigate();
+  const emailInput = useInput('', emailValidator);
+  const passwordInput = useInput('', passwordValidator);
+  const passwordCheckInput = useInput('', (v) => passwordInput.value === v);
 
   return (
     <div className={styles.container}>
       <h1>Register</h1>
-      <TextField name="email" variant="outlined" label="Email" required fullWidth />
+      <TextField
+        {...emailInput}
+        error={emailInput.value.length > 0 && !emailInput.isValid}
+        name="email"
+        variant="outlined"
+        label="Email"
+        required
+        fullWidth
+      />
 
       <TextField
+        {...passwordInput}
+        error={passwordInput.value.length > 0 && !passwordInput.isValid}
         name="password"
         type="password"
         variant="outlined"
         label="Password"
         required
         fullWidth
-        sx={{ marginTop: '1rem' }}
+        sx={{ marginTop: '2rem' }}
       />
 
       <TextField
+        {...passwordCheckInput}
+        error={passwordCheckInput.value.length > 0 && !passwordCheckInput.isValid}
         variant="outlined"
         label="Confirm Password"
         type="password"
+        helperText="at least 8 characters"
         required
         fullWidth
-        sx={{ marginTop: '0.25rem' }}
+        sx={{ marginTop: '0.75rem' }}
       />
 
       <Button
+        disabled={!emailInput.isValid || !passwordInput.isValid || !passwordCheckInput.isValid}
         type="submit"
-        disabled={false}
         variant="contained"
         size="large"
         fullWidth
