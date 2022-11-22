a4409dd674af28d602c32e21ab91fd7bfab8ba5f
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-21 23:25:30 -0600

    update redux store, userSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import userReducer from '../features/user/userSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    user: userReducer,
   },
 });
 
---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface UserState {
+  id: string;
+  email: string;
+  type: string;
+}
+
+const initialState: UserState = {
+  id: '',
+  email: '',
+  type: '',
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<UserState>) => {
+      state.id = action.payload.id;
+      state.email = action.payload.email;
+      state.type = action.payload.type;
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    logout: (state) => {
+      state = initialState;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
