cc4beac51dcfb9f98529ef44c2ec67bd5965b2ea
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-21 23:29:32 -0600

    done guest enter

--- src/components/landing-page/landing-page-index.tsx
+++ src/components/landing-page/landing-page-index.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import styles from './landing-page-index.module.css';
+import { userAPI } from '../../apis';
+import { AxiosResponse } from 'axios';
+import { useAppDispatch } from '../../app/hooks';
+
+interface loginResponseData {
+  id: string;
+  email: string;
+  type: string;
+  token: string;
+}
 
 function LandingPageIndex() {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  async function handleGuestEnter() {
+    try {
+      const response: AxiosResponse<loginResponseData> = await userAPI().get('/guest');
+      const { id, email, type, token } = response.data;
+
+      dispatch({ type: 'user/login', payload: { id, email, type } });
+      sessionStorage.setItem('token', token);
+
+      navigate('/main');
+      //
+    } catch (e: any) {
+      alert(e.toString());
+    }
+  }
 
   return (
     <div className={styles.container}>
-      <h1>Task Board</h1>
       <h3>Organize your tasks</h3>
       <div className={styles.btns}>
         <div className={styles.btns__row1}>
@@ -20,7 +45,7 @@ function LandingPageIndex() {
           </Button>
         </div>
         <div className={styles.btns__row2}>
-          <Button onClick={() => console.log('register')} variant="text">
+          <Button onClick={handleGuestEnter} variant="text">
             Guest Enter
           </Button>
         </div>
--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import React from 'react';
 import styles from './landing-page.module.css';
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.page}>
       <div className={styles.container}>
+        <h1 className={styles.title} onClick={() => navigate('/')}>
+          Task Board
+        </h1>
         <Outlet />
       </div>
     </div>
