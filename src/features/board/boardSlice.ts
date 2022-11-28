a6feab0823372a79b9eaf398c27cd127b3ac622a
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-27 18:38:53 -0600

    update board reducer - task drag

--- src/features/board/boardSlice.ts
+++ src/features/board/boardSlice.ts
@@ -1,6 +1,7 @@
 import { TaskDTO } from './../../types/task';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TaskListDetailDTO } from '../../types/taskList';
+import { DraggableLocation } from 'react-beautiful-dnd';
 
 export interface BoardState {
   id: string;
@@ -25,7 +26,7 @@ export const boardListSlice = createSlice({
     updateBoardTitle: (state, action: PayloadAction<string>) => {
       state.title = action.payload;
     },
-    //
+    // ** TaskLists **
     addList: (state, action: PayloadAction<TaskListDetailDTO>) => {
       state.lists.push(action.payload);
     },
@@ -35,28 +36,43 @@ export const boardListSlice = createSlice({
       state.lists.forEach((el) => (el.id === id ? (el.title = title) : null));
     },
     //
-    updateListOrder: (state, action: PayloadAction<{ currIdx: number; targetIdx: number }>) => {
-      const { currIdx, targetIdx } = action.payload;
-      reorderArray(state.lists, currIdx, targetIdx);
-    },
-    //
     removeList: (state, action: PayloadAction<{ id: string }>) => {
       state.lists.filter((el) => el.id !== action.payload.id);
     },
-    //
+    // ** Tasks **
     addTask: (state, action: PayloadAction<{ listIndex: number; data: TaskDTO }>) => {
       const { listIndex, data } = action.payload;
       state.lists[listIndex].tasks.push(data);
     },
+    //
+    taskDrag: (state, action: PayloadAction<{ src: DraggableLocation; dest: DraggableLocation }>) => {
+      const { src, dest } = action.payload;
+
+      if (src.droppableId === dest.droppableId) {
+        const tasks = state.lists[Number(src.droppableId)].tasks;
+        state.lists[Number(src.droppableId)].tasks = reorderArray(tasks, src.index, dest.index);
+        return;
+      }
+
+      const srcTasks = state.lists[Number(src.droppableId)].tasks;
+      const destTasks = state.lists[Number(dest.droppableId)].tasks;
+      const taskToMove = srcTasks[src.index];
+      state.lists[Number(src.droppableId)].tasks = srcTasks.filter((t, idx) => idx !== src.index);
+      state.lists[Number(dest.droppableId)].tasks = [
+        ...destTasks.slice(0, dest.index),
+        taskToMove,
+        ...destTasks.slice(dest.index),
+      ];
+    },
   },
 });
 
-export const { setBoard, addList, updateListTitle, updateListOrder, removeList } = boardListSlice.actions;
+export const { setBoard, addList, updateListTitle, removeList, taskDrag } = boardListSlice.actions;
 export default boardListSlice.reducer;
 
-function reorderArray(array: any[], idx: number, targetIdx: number): void {
+function reorderArray(array: any[], idx: number, targetIdx: number): any[] {
   if (idx < 0 || targetIdx < 0 || idx >= array.length || targetIdx >= array.length) {
-    return;
+    return [];
   }
 
   if (idx < targetIdx) {
@@ -68,6 +84,8 @@ function reorderArray(array: any[], idx: number, targetIdx: number): void {
       swap(i, i - 1);
     }
   }
+
+  return array;
 
   function swap(idx1: number, idx2: number) {
     const temp = array[idx1];
